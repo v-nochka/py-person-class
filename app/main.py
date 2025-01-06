8111e9263a1e452e0824118ad32266d63a349842
Nica <veronichkapapizh@gmail.com>
2025-01-06 13:24:35 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -9,11 +9,20 @@ class Person:
 
 def create_person_list(people: list[dict]) -> list:
     person_list = [Person(each["name"], each["age"]) for each in people]
+    # Link wife/husband relationships
     for each in people:
         person_instance = Person.people[each["name"]]
         if "wife" in each and each["wife"] is not None:
-            person_instance.wife = Person.people[each["wife"]]
+            if each["wife"] in Person.people:
+                person_instance.wife = Person.people[each["wife"]]
+            else:
+                print(f"Warning: Wife {each["wife"]} for {each["name"]} "
+                      f"not found.")
         if "husband" in each and each["husband"] is not None:
-            person_instance.husband = Person.people[each["husband"]]
+            if each["husband"] in Person.people:
+                person_instance.husband = Person.people[each["husband"]]
+            else:
+                print(f"Warning: Husband {each["husband"]} for {each["name"]} "
+                      f"not found.")
 
     return person_list
